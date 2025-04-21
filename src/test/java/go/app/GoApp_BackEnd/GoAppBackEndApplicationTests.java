ecab16652f2696f1f13cc76dde1f075d54064da1
tchami <131756888+tchamianest@users.noreply.github.com>
2025-04-21 18:48:57 +0200

    setup:backend in java with hello world

---
+++ src/test/java/go/app/GoApp_BackEnd/GoAppBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package go.app.GoApp_BackEnd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoAppBackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
